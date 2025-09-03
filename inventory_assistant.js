95d64a5aacdd153176c10749bb5d63961a5e533e
Austin Lee <austinlee@usf.edu>
2025-09-03 11:36:11 -0400

    copy paste og into new

---
+++ inventory_assistant.js
@@ -0,0 +1,21 @@
+// Coding Challenge 2b
+const itemName = ("USB-C Cable")
+const unitCost = 2
+const currentStock = 250
+const reorderLevel = 125
+const targetStock = 300
+const weeklyDemand = 50
+const supplierLeadTimeWeeks = .5
+
+let weeksOfCover= weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity
+let stockDeficit= Math.max(0, targetStock - currentStock)
+let reorderQuantity= (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0
+let estimatedReorderCost= reorderQuantity * unitCost
+let reorderNow= currentStock<= reorderLevel || weeksOfCover <supplierLeadTimeWeeks
+
+console.log(itemName)
+console.log(Math.max (weeksOfCover))
+console.log(reorderNow)
+console.log(reorderLevel)
+console.log(reorderQuantity.toFixed(2))
+console.log("Estimated Cost: $" + estimatedReorderCost.toFixed(2))
---
+++ pricing_assistant.js
@@ -0,0 +1,25 @@
+// Coding Challenge 2a
+let productName= "potato";
+const costPerUnit = 12.5
+costPerUnit
+const basePrice = 19.99;
+const discountRate = .25;
+const salesTaxRate = .07;
+const rent = 1200;
+const utilities = 250;
+const platform = 100;
+let fixMonthlyCosts=rent+utilities+platform
+
+let discountPrice= basePrice*(1-discountRate)
+let finalPriceWithTax= discountPrice*(1+salesTaxRate)
+let profitPerUnit=finalPriceWithTax-costPerUnit
+let breakEvenUnits= Math.ceil(fixMonthlyCosts/profitPerUnit)
+let isProfitablePerUnit = profitPerUnit > 0
+
+console.log(productName)
+console.log(discountRate)
+console.log(finalPriceWithTax)
+console.log(profitPerUnit)
+console.log(breakEvenUnits)
+console.log(breakEvenUnits)
+console.log(isProfitablePerUnit)
